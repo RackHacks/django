87daf906b305b528a41b2eda56b37f40fb35f80f
Andres Torres <andres.torres.marroquin@gmail.com>
2011-07-18 14:38:45 -0500

    Added support for lowercase and uppercase RFC's and CURP's

--- django/contrib/localflavor/mx/forms.py
+++ django/contrib/localflavor/mx/forms.py
@@ -5,6 +5,9 @@ Mexican-specific form helpers.
 
 from django.forms.fields import Select, RegexField
 from django.utils.translation import ugettext_lazy as _
+import re
+
+date_re = r'\d{2}((01|03|05|07|08|10|12)(0[1-9]|[12]\d|3[01])|02(0[1-9]|[12]\d)|(04|06|09|11)(0[1-9]|[12]\d|30))'
 
 class MXStateSelect(Select):
     """
@@ -42,8 +45,13 @@ class MXRFCField(RegexField):
     }
 
     def __init__(self, *args, **kwargs):
-        regex = ur'^[a-zA-Z&Ññ]{3,4}\d{2}((01|03|05|07|08|10|12)(0[1-9]|[12]\d|3[01])|02(0[1-9]|[12]\d)|(04|06|09|11)(0[1-9]|[12]\d|30))[a-zA-Z0-9]{3}$'
-        super(MXRFCField, self).__init__(regex, *args, **kwargs)
+        rfc_re = re.compile(ur'^[A-Z&Ññ]{3,4}%s[A-Z0-9]{3}$' % date_re, re.IGNORECASE)
+        super(MXRFCField, self).__init__(rfc_re, *args, **kwargs)
+
+    def clean(self, value):
+        value = super(MXRFCField, self).clean(value)
+        value = value.upper()
+        return value
 
 class MXCURPField(RegexField):
     """
@@ -57,5 +65,13 @@ class MXCURPField(RegexField):
     }
 
     def __init__(self, *args, **kwargs):
-        regex = ur'^[A-Z]{4}\d{2}((01|03|05|07|08|10|12)(0[1-9]|[12]\d|3[01])|02(0[1-9]|[12]\d)|(04|06|09|11)(0[1-9]|[12]\d|30))[HM](AS|BC|BS|CC|CL|CM|CS|CH|DF|DG|GT|GR|HG|JC|MC|MN|MS|NT|NL|OC|PL|QT|QR|SP|SL|SR|TC|TS|TL|VZ|YN|ZS|NE)[B-DF-HJ-NP-TV-Z]{3}\d{2}$'
-        super(MXCURPField, self).__init__(regex, *args, **kwargs)
+        states_re = r'(AS|BC|BS|CC|CL|CM|CS|CH|DF|DG|GT|GR|HG|JC|MC|MN|MS|NT|NL|OC|PL|QT|QR|SP|SL|SR|TC|TS|TL|VZ|YN|ZS|NE)'
+        consonants_re = r'[B-DF-HJ-NP-TV-Z]'
+        curp_re = ur'^[A-Z]{4}%s[HM]%s%s{3}\d{2}$' % (date_re, states_re, consonants_re)
+        curp_re = re.compile(curp_re, re.IGNORECASE)
+        super(MXCURPField, self).__init__(curp_re, *args, **kwargs)
+
+    def clean(self, value):
+        value = super(MXCURPField, self).clean(value)
+        value = value.upper()
+        return value

78baa039d289b5fdbda03cc5dcf3292bd76d9c3a
Gerardo Orozco <gerardo.orozco.mosqueda@gmail.com>
2011-07-20 19:28:46 -0500

    Localflavor MXFields almost finished

--- django/contrib/localflavor/mx/forms.py
+++ django/contrib/localflavor/mx/forms.py
@@ -3,10 +3,12 @@
 Mexican-specific form helpers.
 """
 
+from django.forms import ValidationError
 from django.forms.fields import Select, RegexField, CharField
 from django.utils.translation import ugettext_lazy as _
+from django.core.validators import EMPTY_VALUES
 from django.contrib.localflavor.mx.mx_states import STATE_CHOICES
-from datetime.datetime import date
+from datetime import date
 import re
 
 DATE_RE = r'\d{2}((01|03|05|07|08|10|12)(0[1-9]|[12]\d|3[01])|02(0[1-9]|[12]\d)|(04|06|09|11)(0[1-9]|[12]\d|30))'
@@ -16,12 +18,14 @@ This is the list of inconvenient words according to the `Anexo IV` of the
 document described in the next link:
     www.sisi.org.mx/jspsi/documentos/2005/seguimiento/06101/0610100162005_065.doc
 """
+
 RFC_INCONVENIENT_WORDS = [
-    u'BUEI', u'BUEY', u'CACA', u'CACO', u'CAGA', u'CAGO', u'CAKA', u'CAKO', u'COGE',
-    u'COJA', u'COJE', u'COJI', u'COJO', u'CULO', u'FETO', u'GUEY', u'JOTO', u'KACA',
-    u'KACO', u'KAGA', u'KAGO', u'KOGE', u'KOJO', u'KAKA', u'KULO', u'MAME', u'MAMO',
-    u'MEAR', u'MEAS', u'MEON', u'MION', u'MOCO', u'MULA', u'PEDA', u'PEDO', u'PENE',
-    u'PUTA', u'PUTO', u'QULO', u'RATA', u'RUIN'
+    u'BUEI', u'BUEY', u'CACA', u'CACO', u'CAGA', u'CAGO', u'CAKA', u'CAKO',
+    u'COGE', u'COJA', u'COJE', u'COJI', u'COJO', u'CULO', u'FETO', u'GUEY',
+    u'JOTO', u'KACA', u'KACO', u'KAGA', u'KAGO', u'KOGE', u'KOJO', u'KAKA',
+    u'KULO', u'MAME', u'MAMO', u'MEAR', u'MEAS', u'MEON', u'MION', u'MOCO',
+    u'MULA', u'PEDA', u'PEDO', u'PENE', u'PUTA', u'PUTO', u'QULO', u'RATA',
+    u'RUIN'
 ]
 
 """
@@ -30,15 +34,17 @@ document described in the next link:
     http://portal.veracruz.gob.mx/pls/portal/url/ITEM/444112558A57C6E0E040A8C02E00695C
 """
 CURP_INCONVENIENT_WORDS = [
-   u'BACA', u'BAKA', u'BUEI', u'BUEY', u'CACA', u'CACO', u'CAGA', u'CAGO', u'CAKA',
-   u'CAKO', u'COGE', u'COGI', u'COJA', u'COJE', u'COJI', u'COJO', u'COLA', u'CULO',
-   u'FALO', u'FETO', u'GETA', u'GUEI', u'GUEY', u'JETA', u'JOTO', u'KACA', u'KACO',
-   u'KAGA', u'KAGO', u'KAKA', u'KAKO', u'KOGE', u'KOGI', u'KOJA', u'KOJE', u'KOJI',
-   u'KOJO', u'KOLA', u'KULO', u'LILO', u'LOCA', u'LOCO', u'LOKA', u'LOKO', u'MAME',
-   u'MAMO', u'MEAR', u'MEAS', u'MEON', u'MIAR', u'MION', u'MOCO', u'MOKO', u'MULA',
-   u'MULO', u'NACA', u'NACO', u'PEDA', u'PEDO', u'PENE', u'PIPI', u'PITO', u'POPO',
-   u'PUTA', u'PUTO', u'QULO', u'RATA', u'ROBA', u'ROBE', u'ROBO', u'RUIN', u'SENO',
-   u'TETA', u'VACA', u'VAGA', u'VAGO', u'VAKA', u'VUEI', u'VUEY', u'WUEI', u'WUEY'
+   u'BACA', u'BAKA', u'BUEI', u'BUEY', u'CACA', u'CACO', u'CAGA', u'CAGO',
+   u'CAKA', u'CAKO', u'COGE', u'COGI', u'COJA', u'COJE', u'COJI', u'COJO',
+   u'COLA', u'CULO', u'FALO', u'FETO', u'GETA', u'GUEI', u'GUEY', u'JETA',
+   u'JOTO', u'KACA', u'KACO', u'KAGA', u'KAGO', u'KAKA', u'KAKO', u'KOGE',
+   u'KOGI', u'KOJA', u'KOJE', u'KOJI', u'KOJO', u'KOLA', u'KULO', u'LILO',
+   u'LOCA', u'LOCO', u'LOKA', u'LOKO', u'MAME', u'MAMO', u'MEAR', u'MEAS',
+   u'MEON', u'MIAR', u'MION', u'MOCO', u'MOKO', u'MULA', u'MULO', u'NACA',
+   u'NACO', u'PEDA', u'PEDO', u'PENE', u'PIPI', u'PITO', u'POPO', u'PUTA',
+   u'PUTO', u'QULO', u'RATA', u'ROBA', u'ROBE', u'ROBO', u'RUIN', u'SENO',
+   u'TETA', u'VACA', u'VAGA', u'VAGO', u'VAKA', u'VUEI', u'VUEY', u'WUEI',
+   u'WUEY'
 ]
 
 class MXStateSelect(Select):
@@ -73,24 +79,36 @@ class MXRFCField(RegexField):
     """
     default_error_messages = {
         'invalid': _(u'Enter a valid RFC.'),
-        'inconvenient': _(u'Enter a RFC with no inconvenient business name.'),
     }
-    
-    def __init__(self, min_length=12, max_length=13, *args, **kwargs):
-        rfc_re = re.compile(ur'^[A-Z&Ññ]{3,4}%s[A-Z0-9]{2}[0-9A]$' % DATE_RE,
+
+    def __init__(self, min_length=9, max_length=13, *args, **kwargs):
+        rfc_re = re.compile(ur'^([A-Z&Ññ]{3}|[A-Z][AEIOU][A-Z]{2})%s([A-Z0-9]{2}[0-9A])?$' % DATE_RE,
                             re.IGNORECASE)
         super(MXRFCField, self).__init__(rfc_re, min_length=min_length,
                                          max_length=max_length, *args, **kwargs)
 
     def clean(self, value):
         value = super(MXRFCField, self).clean(value)
+        if value in EMPTY_VALUES:
+            return u''
         value = value.upper()
-        if not value[-1] == self._checksum(value[:-1]):
+        if self._has_homoclave(value):
+            if not value[-1] == self._checksum(value[:-1]):
+                raise ValidationError(self.default_error_messages['invalid'])
+        if self._has_inconvenient_word(value):
             raise ValidationError(self.default_error_messages['invalid'])
-        if self._has_inconvenient_word(rfc):
-            raise ValidationError(self.default_error_messages['inconvenient'])
         return value
-    
+
+    def _has_homoclave(self, rfc):
+        """
+        This check is done due to the existance of RFCs without a `homoclave`
+        since the current algorithm to calculate it had not been created for
+        the first RFCs ever in mexico.
+        """
+        rfc_without_homoclave_re = re.compile(ur'^[A-Z&Ññ]{3,4}%s$' % DATE_RE,
+                                              re.IGNORECASE)
+        return not rfc_without_homoclave_re.match(rfc)
+
     def _checksum(self, rfc):
         """
         More info about this procedure:
@@ -109,12 +127,10 @@ class MXRFCField(RegexField):
             return u'0'
         else:
             return unicode(checksum)
-    
+
     def _has_inconvenient_word(self, rfc):
-        if len(rfc) is 13:
-            business_name = rfc[:4]
-            return business_name in RFC_INCONVENIENT_WORDS
-        return False
+        first_four = rfc[:4]
+        return first_four in RFC_INCONVENIENT_WORDS
 
 class MXCURPField(RegexField):
     """
@@ -125,13 +141,12 @@ class MXCURPField(RegexField):
     """
     default_error_messages = {
         'invalid': _('Enter a valid CURP.'),
-        'inconvenient': _(u'Enter a CURP with no inconvenient name.'),
     }
 
     def __init__(self, min_length=18, max_length=18, *args, **kwargs):
         states_re = r'(AS|BC|BS|CC|CL|CM|CS|CH|DF|DG|GT|GR|HG|JC|MC|MN|MS|NT|NL|OC|PL|QT|QR|SP|SL|SR|TC|TS|TL|VZ|YN|ZS|NE)'
         consonants_re = r'[B-DF-HJ-NP-TV-Z]'
-        curp_re = ur'^[A-Z]{4}%s[HM]%s%s{3}[0-9A-Z]\d$' % (DATE_RE, states_re,
+        curp_re = ur'^[A-Z][AEIOU][A-Z]{2}%s[HM]%s%s{3}[0-9A-Z]\d$' % (DATE_RE, states_re,
                                                       consonants_re)
         curp_re = re.compile(curp_re, re.IGNORECASE)
         super(MXCURPField, self).__init__(curp_re, min_length=min_length,
@@ -139,26 +154,25 @@ class MXCURPField(RegexField):
 
     def clean(self, value):
         value = super(MXCURPField, self).clean(value)
+        if value in EMPTY_VALUES:
+            return u''
         value = value.upper()
         if value[-1] != self._checksum(value[:-1]):
             raise ValidationError(self.default_error_messages['invalid'])
-        if self._has_inconvenient_word(rfc):
-            raise ValidationError(self.default_error_messages['inconvenient'])
+        if self._has_inconvenient_word(value):
+            raise ValidationError(self.default_error_messages['invalid'])
         return value
-    
+
     def _checksum(self, value):
-        curp_chars = u'0123456789ABCDEFGHIJKLMN&OPQRSTUVWXYZ'
-        factor = 19
-        sum_ = 0
-        for i in range(factor):
-            index = curp_chars.index(value[i:i+1])
-            factor -= 1
-            sum_ += index * factor
-        checksum = 10 - sum_ % 10
+        chars = u'0123456789ABCDEFGHIJKLMN&OPQRSTUVWXYZ'
+        
+        s = sum(i * chars.index(c) for i, c in zip(reversed(xrange(19)), value))
+        checksum = 10 - s % 10
+        
         if checksum == 10:
           return u'0'
         return unicode(checksum)
-    
+
     def _has_inconvenient_word(self, curp):
-        name = curp[:4]
-        return name in CURP_INCONVENIENT_WORDS
+        first_four = curp[:4]
+        return first_four in CURP_INCONVENIENT_WORDS
